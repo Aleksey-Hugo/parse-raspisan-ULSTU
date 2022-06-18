b07da9df968b014f30774cec190ae3e3e593c158
Aleksey-Hugo <62599558+Aleksey-Hugo@users.noreply.github.com>
2022-06-18 16:48:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ def start(message):
         message.from_user.last_name,
         message.from_user.username,
         now)
-        bot.send_message(message.from_user.id, "Напиши мне номер своей группы");
+        bot.send_message(message.from_user.id, "Привет я телеграмм бот для информационной поддержки студентов. Я умею выдвать расписание на выбранный день. А также на выбранные недели.Помимо данного функционала я умею выдавать новости колледжа и список нормативных документов");
         bot.register_next_step_handler(message, get_group);
     else:
         bot.send_message(message.from_user.id, 'Для начала выбери или напиши команду "/start"')
